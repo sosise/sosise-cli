15c086e12a656042f2dd1ce952f1516433de9c44
Igor Kov <igor875126@gmail.com>
2021-01-13 14:16:55 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+## Installation
+Install `sosise-cli` via `npm` or `yarn` globally.
+
+```sh
+npm i sosise-cli -g
+```
+
+## Usage
+Use `new` to generate your project.
+
+```sh
+sosise new <name>
+```
+
+## License
+[MIT](LICENSE)
---
+++ src/sosise-cli.ts
@@ -0,0 +1,81 @@
+import { Command } from 'commander';
+import colors from 'colors';
+import fs from 'fs';
+import figlet from 'figlet';
+import { execSync } from 'child_process';
+
+// Specify sosise git clone path
+const gitClonePath = 'https://github.com/sosise/sosise.git';
+
+// Initialize commander
+const program = new Command();
+program.description(colors.yellow('Sosise installer'));
+
+// Register new command
+program
+    .command('new <name>')
+    .description('Sosise installer')
+    .action((name) => {
+        // Output
+        console.log(colors.blue(figlet.textSync('Sosise')));
+        console.log(colors.green('Crafting application...'));
+
+        // Initialize new project path
+        const projectPath = `${process.cwd()}/${name}`;
+
+        // Do not proceed if project path is not empty
+        if (fs.existsSync(projectPath)) {
+            console.log(colors.red(`Application already exists! ${projectPath}`));
+            process.exit(1);
+        }
+
+        // Clone project
+        try {
+            execSync(`git clone ${gitClonePath} ${name} &> /dev/null`, {
+                encoding: 'utf-8'
+            });
+            console.log(colors.green('Project cloned'));
+        } catch (error) {
+            console.log(colors.red('Error occured during cloning sosise, detailed information below'));
+            console.log(colors.red(error));
+        }
+
+        // Copy .env.example to .env
+        fs.copyFileSync(projectPath + '/.env.example', projectPath + '/.env');
+        console.log(colors.green('Copied .env.example -> .env'));
+
+        // Replace text in .env
+        const envContent = fs.readFileSync(projectPath + '/.env', 'utf-8');
+        envContent.replace(/Sosise/g, name);
+        fs.writeFileSync(projectPath + '/.env', envContent);
+
+        // Remove .git folder
+        fs.rmdirSync(projectPath + '/.git', { recursive: true });
+
+
+        try {
+            // Do npm install
+            execSync(`cd ${projectPath} ; npm install --quiet `, { stdio: [0, 1, 2] });
+            console.log(colors.green('Npm packages installed'));
+
+            // Build application
+            execSync(`cd ${projectPath} ; npm run build `, { stdio: [0, 1, 2] });
+            console.log(colors.green('Application has been built'));
+        } catch (error) {
+            console.log(colors.red('Error occured during npm install, detailed information below'));
+            console.log(colors.red(error));
+        }
+
+        console.log('');
+        console.log('');
+        console.log('');
+        console.log(colors.blue('Your application is ready, have a nice day!'));
+        console.log(colors.blue('You now can run it now'));
+        console.log(colors.blue(`cd ${projectPath} ; npm run serve`));
+        console.log('');
+        console.log('');
+        console.log('');
+    });
+
+// Parse cli arguments
+program.parse(process.argv);
